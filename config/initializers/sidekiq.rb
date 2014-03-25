634f9aa7516968f20adba43dc1a089caf5144189
Jiri Kolarik <jiri.kolarik@imin.cz>
2014-03-25 01:38:59 +0100

    Only if web ui is required

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 if defined? Sidekiq
   Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
     [user, password] == [Foundationstone::Config.sidekiq_login, Foundationstone::Config.sidekiq_pass]
-  end
+  end if defined?(Sidekiq::Web)
 
   Sidekiq.configure_server do |config|
     config.redis = { url: 'redis://localhost:6379/0', namespace: "project:#{Foundationstone::Config.unique_name.parameterize}" }
