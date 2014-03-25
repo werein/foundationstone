a16e428cf916a3f22e38d84a28b7bce6f656da5e
Jiri Kolarik <jiri.kolarik@imin.cz>
2014-03-25 01:43:48 +0100

    Sidekiq can be running without web admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Foundationstone::Engine.routes.draw do
   scope '(:locale)', locale: Regexp.new(I18n.available_locales.join('|')) do
-    mount Sidekiq::Web => '/sidekiq' if defined? Sidekiq
+    mount Sidekiq::Web => '/sidekiq' if defined? Sidekiq::Web
     mount Ckeditor::Engine => '/ckeditor' if defined? Ckeditor
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin' if defined? RailsAdmin
   end
